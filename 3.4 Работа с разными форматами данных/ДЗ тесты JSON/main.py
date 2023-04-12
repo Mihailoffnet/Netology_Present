f1c547810e750024b74b899593697a709a2890e2
Mihailoff <mihailoff@inbox.ru>
2023-04-12 20:32:09 +0500

    add

--- 3.4 Работа с разными форматами данных/ДЗ тесты JSON/main.py
+++ 3.4 Работа с разными форматами данных/ДЗ тесты JSON/main.py
@@ -45,52 +45,48 @@ if __name__ == '__main__':
 	print(read_json('newsafr.json'))
 
 
-# import json
+import sys
 
-# # def read_json(file_path, word_max_len=6, top_words_amt=10):
-# #       with open('newsafr.json') as f:
-# #         json_data = json.load(f)
-# #     news_list = json_data["rss"]["channel"]["items"]
+# работа с xml
+import xml.etree.ElementTree as ET
 
+def read_xml(file_path, word_max_len=6, top_words_amt=10):
+    #  открытие файла
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file_path, parser)
 
-# word_max_len=6
-# top_words_amt=10
-# file_path = 'newsafr.json'
+    # как получить информацию о теге xml
+    root = tree.getroot()
+    
+    # Составим список из слов, длинее 6 символов:
 
-# # откроем файл и прочитаем его в список словарей
+    word_list = []
+    news_list = root.findall("channel/item")
+    for news in news_list:
+        list = news.find("description").text.split(sep = ' ')
+        for word in list:
+            if len(word) > word_max_len:
+                word_list.append(word)
+    world_set = set(word_list)
 
-# with open('newsafr.json') as f:
-# 	json_data = json.load(f)
+	# составим словарь количество : слово
 
-# news_list = json_data['rss']['channel']['items'] # news_list type list
+    word_dict = {}
 
+    for s_word in world_set:
+        count = word_list.count(s_word)
+        word_dict[s_word] = count
+    word_dict_sorted = dict(sorted(word_dict.items(), key=lambda item: item[1], reverse=True)[:top_words_amt])
 
-# # составим список из всех слов длинее 6 символов в новостях:
+    # print(word_dict_sorted)
 
-# word_list = []
+    # составим список из первых десяти элементов и выведем их на печать:
 
-# for item in news_list:
-# 	item_list = item['description'].split(sep = ' ')
-# 	for word in item_list:
-# 		if len(word) > word_max_len:
-# 			word_list.append(word)
-# # составим словарь количество : слово
-# word_dict = {}
+    topword_list =[]
+    for topword in word_dict_sorted.keys():
+	    topword_list.append(topword)
 
-# for s_word in word_list:
-# 	count = word_list.count(s_word)
-# 	if s_word in word_dict:
-# 		print(f'дубль слово {s_word}')
-# 		continue
-# 	word_dict[s_word] = count
+    return topword_list
 
-# word_dict_sorted = dict(sorted(word_dict.items(), key=lambda item: item[1], reverse=True)[:top_words_amt])
-# # print(word_dict_sorted)
-
-# # составим список из первых десяти элементов и выведем их на печать:
-
-# topword_list =[]
-# for topword in word_dict_sorted.keys():
-# 	topword_list.append(topword)
-
-# print(topword_list)
+if __name__ == '__main__':
+    print(read_xml('newsafr.xml'))
