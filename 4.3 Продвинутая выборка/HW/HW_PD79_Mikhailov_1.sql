5ba21b1ad9ab0dcfe802c4b85b8f98a421db9f09
Mihailoff <mihailoff@inbox.ru>
2023-06-05 21:52:19 +0500

    add

--- 4.3 Продвинутая выборка/HW/HW_PD79_Mikhailov_1.sql
+++ 4.3 Продвинутая выборка/HW/HW_PD79_Mikhailov_1.sql
@@ -59,3 +59,72 @@ INSERT INTO track_collection VALUES (1, 1);
 INSERT INTO track_collection VALUES (6, 2);
 INSERT INTO track_collection VALUES (7, 3);
 INSERT INTO track_collection VALUES (5, 4);
+
+-- НОВАЯ ВЫБОРКА
+
+-- Добавляем жанры
+INSERT INTO jenre(jenre_name) values
+('Rock'),
+('Metall'),
+('POP'),
+('Hip-Hop'),
+('RAP');
+
+-- Добавляем исполнителей
+INSERT INTO singer(singer_name) VALUES 
+('Metallica'),
+('Eminem'),
+('Kurt Cobain'),
+('Morgenstern'),
+('Britney Spears');
+
+
+-- Делаем связку исполнителей с жанрами
+INSERT INTO singer_jenre values
+(1, 3),
+(2, 1),
+(3, 5),
+(4, 2),
+(5, 4);
+
+-- Добавляем альбомы
+INSERT INTO album(album_name, album_years) VALUES 
+('Cadillac', 2021), -- Морген
+('Circus', 2008), -- Спирс
+('In the Zone', 2003), -- Спирс
+('Ride the Lightning', 1986), -- Металлика
+('Kill Em All', 1983), -- Металлика
+('The Slim Shady LP', 1991), -- Эминем
+('Nevermind', 1991); --Кобейн 
+
+-- Делаем связку исполнителей с альбомами
+INSERT INTO singer_album values
+(1, 4),
+(2, 5),
+(3, 5),
+(4, 1),
+(5, 1),
+(6, 2),
+(7, 3);
+
+-- Добавляем треки
+INSERT INTO track VALUES (1, 'Мой Cadillac', 1, 192); -- Морген
+INSERT INTO track VALUES (2, 'In Bloom', 7, 255); -- Кобейн
+INSERT INTO track VALUES (3, 'Hold It Against Me', 3, 310); -- Спирс
+INSERT INTO track VALUES (4, 'Escape', 4, 261); -- Металлика
+INSERT INTO track VALUES (5, 'My Name Is', 6, 268); -- Эминем
+INSERT INTO track VALUES (6, 'Jump In The Fire', 5, 281); -- Металлика
+INSERT INTO track VALUES (7, 'Love Story (Taylor Swift)', 2, 207); -- Спирс
+INSERT INTO track VALUES (8, 'Stay Away', 7, 211); -- Кобейн
+
+-- Добавляем сборники
+INSERT INTO collection VALUES (1, 'TOP 100 2023', 2023); -- Морген
+INSERT INTO collection VALUES (2, 'Rock Ballads', 2002); -- Металлика
+INSERT INTO collection VALUES (3, 'Disco 2000', 2018); -- Спирс
+INSERT INTO collection VALUES (4, 'RAPsody', 2020); -- Эминем
+
+-- Делаем связку сборника с треком
+INSERT INTO track_collection VALUES (1, 1);
+INSERT INTO track_collection VALUES (6, 2);
+INSERT INTO track_collection VALUES (7, 3);
+INSERT INTO track_collection VALUES (5, 4);
